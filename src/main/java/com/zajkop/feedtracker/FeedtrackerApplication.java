182cff8cf6bdecd150822c2a2fb1d1301d21e56b
zajkop <zajkopatryk97@gmail.com>
2024-06-15 21:58:52 +0200

    project init

---
+++ src/main/java/com/zajkop/feedtracker/FeedtrackerApplication.java
@@ -0,0 +1,13 @@
+package com.zajkop.feedtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedtrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeedtrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zajkop/feedtracker/FeedtrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zajkop.feedtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedtrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
